301e30f9e85593a5b7304d08ac6858e9f0377562
Artsem Zgharou <artsem.zhgarou@glera-games.com>
2022-04-12 12:11:04 +0200

    test phone and email

--- src/app/pages/Home/Home.jsx
+++ src/app/pages/Home/Home.jsx
@@ -1,19 +1,35 @@
 import React from 'react'
 import {Alert, Grid} from '@mui/material'
+import {useSelector} from 'react-redux'
 import {Card, PageLayout} from '../../components'
 import global from '../../../config/constants/helpOffer.json'
 import {StyledPageHeaderText, StyledPromoWrapper, StyledText} from './styles'
 
-import {useGetOffersQuery} from '../../../features/api/apiSlice'
 import {Filter} from './components'
 import OffersSkeleton from './components/OffersSkeleton/OffersSkeleton'
 import {Paginator} from '../../components/Paginator'
+import {selectOffers, useGetOffersQuery} from '../../../features/api/apiSlice'
 
 const Home = () => {
-  const offersData = useGetOffersQuery()
+  const offers = useGetOffersQuery()
+  const offersFilter = useSelector((state) => state?.offers?.offersFilter)
+  // console.log('OFFERS', offers)
+  // console.log('OFFERS', offersFilter)
+  // console.log(
+  //   'useSelector!!!!!!!!!!!!!!!!!!!',
+  //   useSelector((state) =>
+  //     apiSlice.endpoints.getOffers.select(offersFilter)(state),
+  //   ),
+  // )
+  console.log(
+    'SELECTED',
+    useSelector((state) => selectOffers(state, offersFilter)),
+  )
 
   return (
     <PageLayout>
+      <a href="mailto:noowaay.pl@gmail.com">noowaay.pl@gmail.com</a>
+      <a href="tel:+48537157674">+48537157674</a>
       <StyledPromoWrapper container>
         <StyledPageHeaderText variant="h1">
           {global.home.H1Text}
@@ -23,15 +39,15 @@ const Home = () => {
       <Filter />
       <Paginator topPagination show>
         {/* eslint-disable-next-line no-nested-ternary */}
-        {offersData.isUninitialized || offersData.isLoading ? (
+        {offers.isUninitialized || offers.isLoading ? (
           <OffersSkeleton />
-        ) : offersData.isError ? (
+        ) : offers.isError ? (
           <Grid item xs={12} md={12}>
             <Alert severity="error">Error</Alert>
           </Grid>
         ) : (
           <>
-            {offersData.data?.map((offer) => {
+            {offers.data?.map((offer) => {
               return (
                 <Grid item xs={12} md={4} key={offer.id}>
                   <Card {...offer} />
